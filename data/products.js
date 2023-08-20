08eb0e20f0b7e42d00643de205ce720d73c346de
Maksym <max.sharinov@gmail.com>
2023-08-20 20:03:17 +0300

    feat: optimized images

--- data/products.js
+++ data/products.js
@@ -5,7 +5,8 @@ export default [
     color: 'orange',
     isTextDark: false,
     price: 9.99,
-    description: "The perfect combination of taste and nutrition! Tropicana Pure Premium® Original is 100% pure orange juice, squeezed from fresh-picked oranges and never from concentrate. No wonder it’s the #1 orange juice brand.",
+    description:
+      'The perfect combination of taste and nutrition! Tropicana Pure Premium® Original is 100% pure orange juice, squeezed from fresh-picked oranges and never from concentrate. No wonder it’s the #1 orange juice brand.',
     images: {
       imgMain: 'assets/orange-drink.png',
       imgTop: 'assets/orange-slice.png',
@@ -18,11 +19,11 @@ export default [
     color: 'green',
     isTextDark: true,
     price: 11.99,
-    description: "Piña Colada is a refreshing and delicious drink that combines the flavors of juicy pineapple and creamy coconut. Every sip is a splash of piña paradise that takes you to the tropics.",
+    description:
+      'Piña Colada is a refreshing and delicious drink that combines the flavors of juicy pineapple and creamy coconut. Every sip is a splash of piña paradise that takes you to the tropics.',
     images: {
       imgMain: 'assets/pina.png',
-      imgTop:
-        'https://static.vecteezy.com/system/resources/previews/009/887/404/non_2x/green-leaf-of-palm-tree-on-transparent-background-file-free-png.png',
+      imgTop: 'assets/pina-leaf.webp',
       imgBottom: 'https://www.transparentpng.com/thumb/coconut/V0VM8d-coconut-hd-image.png',
     },
   },
@@ -32,7 +33,8 @@ export default [
     color: 'mango',
     isTextDark: false,
     price: 10.99,
-    description: "Take the taste of delicious fruit juices, put them together in one refreshing blend, and you get Tropicana® Pineapple Mango Splash. A delicious juice drink with plenty of bright, tropical flavors.",
+    description:
+      'Take the taste of delicious fruit juices, put them together in one refreshing blend, and you get Tropicana® Pineapple Mango Splash. A delicious juice drink with plenty of bright, tropical flavors.',
     images: {
       imgMain: 'assets/mango-drink.png',
       imgBottom: 'assets/mango-pineapple.png',
