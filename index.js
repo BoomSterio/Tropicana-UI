ee867dedb109c98be1a90a5ba3f5bd1d7f0f0443
Maksym <max.sharinov@gmail.com>
2023-08-20 20:19:09 +0300

    feat: added smooth scroll polyfill

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import HomePage from './src/HomePage.js'
 import { uiFeatures } from './src/utils.js'
+import smoothscroll from './src/scroll-polyfill.js'
+
+smoothscroll.polyfill();
 
 const homePage = new HomePage()
 homePage.init()
